e0cec2e94f8a4b01bf4e1c705d2a34da79de5356
cristalNgoSIT <ngoc_nm@yahoo.com>
2015-02-09 15:53:52 +0800

    Update PongView.java

--- src/pong/PongView.java
+++ src/pong/PongView.java
@@ -22,7 +22,7 @@ public class PongView extends JFrame {
      * @param model the model for this Pong game
      */
     public PongView(PongModel model) {
-        super("Simple Pong Game to illustrate MVC for ICT2101");
+        super("Simple Pong Game to illustrate MVC for ICT2101 - Blah labj");
         this.model = model;
         flipVertical = false;
 
